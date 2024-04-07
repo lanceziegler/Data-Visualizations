39a633b7ca2063f044dd48151670d04025fbd909
Lance Ziegler <lance.ziegler@hotmail.com>
2024-04-07 16:35:52 -0700

    padding

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import LineGraph from './components/LineGraph';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
+    <main className='flex min-h-screen flex-col items-center justify-center px-24'>
       <div>Hi Krissy</div>
       <LineGraph />
     </main>
